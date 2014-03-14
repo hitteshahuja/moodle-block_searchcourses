12d0b688f0c80a5cf60880a29e544655826b6227
Hittesh Ahuja <hitteshahuja@bath.ac.uk>
2014-03-14 15:39:58 +0000

    Added default value for config_course_count and gave PARAM_INT type to supress error

--- edit_form.php
+++ edit_form.php
@@ -8,6 +8,7 @@ class block_searchcourses_edit_form extends block_edit_form{
 		//Number of courses to show in the drop-down
 		$course_count_attributes = array('style'=>'width:3em');
 		$mform->addElement('text','config_course_count',get_string('course_count_label','block_searchcourses'),$course_count_attributes);
-		$mform->setDefault('course_count',15);
+		$mform->setDefault('config_course_count',15);
+		$mform->setType('config_course_count',PARAM_INT);
 	}
 }
