52e63eaedf4f3a96527e43ff1a78c4105b7244bf
Hittesh Ahuja <h.ahuja@bath.ac.uk>
2015-11-15 19:35:54 +0000

    Made changes to output

--- result.php
+++ result.php
@@ -8,39 +8,44 @@ if(!defined('AJAX_SCRIPT')){
 require_once($CFG->libdir . '/datalib.php');
 global $CFG;
 $USER;
-$query            = $_GET['query'];
+$query            = $_GET['term'];
 $my_courses_flag  = 0;
 $total            = 0;
-$courses['query'] = $query;
+//$courses['query'] = $query;
 $course_count     = 10; // Default value for course result list
 if (!empty($_GET['course_count'])) {
     $course_count = $_GET['course_count'];
 }
-if (($_GET['my_courses_flag']) == "true") {
-    $my_courses_flag    = $_GET['my_courses_flag'];
-    $courses['results'] = enrol_get_my_courses(array('id', 'shortname'), 'visible DESC,sortorder ASC', $course_count);
-    //Once you have the results, filter the ones matching the search query
-    $mycourses          = array();
-    foreach ($courses['results'] as $objCourse) {
-        if (preg_match('/' . $query . '/i', $objCourse->fullname) != 0) {
-            $mycourses[] = $objCourse;
+if(isset($_GET['my_courses_flag'])){
+    if (($_GET['my_courses_flag']) == "true" ) {
+        $my_courses_flag    = $_GET['my_courses_flag'];
+        $courses['results'] = enrol_get_my_courses(array('id', 'shortname'), 'visible DESC,sortorder ASC', $course_count);
+        //Once you have the results, filter the ones matching the search query
+        $mycourses          = array();
+        foreach ($courses['results'] as $objCourse) {
+            if (preg_match('/' . $query . '/i', $objCourse->fullname) != 0) {
+                $mycourses[] = $objCourse;
+            }
         }
+        $courses['results'] = array_values($mycourses);
+        echo json_encode($courses);
+
     }
-    $courses['results'] = array_values($mycourses);
-    echo json_encode($courses);
-    
-} else {
-    $courses['results'] = array_values(get_courses_search(array(
+
+}
+else {
+    $courses = array_values(get_courses_search(array(
         $query
     ), 'fullname ASC', 0, $course_count, $total));
-    if (empty($courses['results'])) {
+    if (empty($courses)) {
         $objCourse          = new stdClass();
         $objCourse->id      = 'na';
         $courses['results'] = array_values(array(
             $objCourse
         ));
-        echo json_encode($courses);
+        echo json_encode([]);
     } else {
         echo json_encode($courses);
     }
 }
+
