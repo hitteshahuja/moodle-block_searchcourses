9ca2e96398bfec80e4fa54381aca937cb8e9f052
hitteshahuja <hitteshahuja@gmail.com>
2016-08-13 23:35:08 +0100

    Added  tooltip support Added course shortname to tooltip

--- amd/src/searchcourses.js
+++ amd/src/searchcourses.js
@@ -6,19 +6,26 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later/**.
  */
 define(['jquery', 'core/config',"jqueryui"], function ($, config,jqui) {
-    var courses = ['Hello'];
     return {
         initialise: function (course_count) {
-            console.log("Initi");
+            $(function () {
+                $(document).tooltip({
+                    content: function () {
+                        return $(this).prop('title');
+                    }
+                });
+            });
+
             $('#course_search_ac .typeahead').autocomplete(
                 {
-                    source: config.wwwroot+"/blocks/searchcourses/ajax/result.php",
+                    source: config.wwwroot+"/blocks/searchcourses/ajax/result.php?course_count="+course_count,
                     minLength:2
                 }
             ).autocomplete( "instance" )._renderItem = function( ul, item ) {
-                console.log(item);
+                var course_url = config.wwwroot+'/course/view.php?id='+item.id;
+                var tooltip_data = "<p><b>Short Name: </b>"+item.shortname+"</p>";
                 return $( "<li>" )
-                    .append( "<a>" + item.id + "<br>" + item.fullname + "</a>" )
+                    .append( "<a id='course-result-node' data-placement='left' data-toggle='tooltip' title='"+tooltip_data+"' href='"+course_url+"'>" + item.fullname + "</a>" )
                     .appendTo( ul );
             };
         }
