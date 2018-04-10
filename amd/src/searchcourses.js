f03e78d1fa5202a7cc60f728a4773816c3c9d733
Hittesh Ahuja <h.ahuja@bath.ac.uk>
2018-04-10 13:17:12 +0100

    Added search and focus event options

--- amd/src/searchcourses.js
+++ amd/src/searchcourses.js
@@ -14,9 +14,24 @@ define(['jquery', 'core/config', 'jqueryui', 'core/templates', 'block_searchcour
                 {
                     source: source,
                     minLength: 2,
+                    search: function (event, ui) {
+                        var loadingImg = document.createElement("img");
+                        $(loadingImg).attr("src", M.util.image_url('/i/ajaxloader', 'moodle'));
+                        $(loadingImg).attr("alt", "Loading");
+                        $(loadingImg).attr("class", "block_searchcourses-loader");
+                        $(loadingImg).insertAfter($(this));
+                    },
+                    response: function (event, ui) {
+                        // Show a loading icon
+                        $('.block_searchcourses-loader').hide();
+
+                    },
+                    focus: function (event, ui) {
+                        console.log("foc");
+                    },
                     select: function (event, ui) {
                         console.log(ui);
-                        getCourseInfo(ui.item);
+                        courseinfo.showcourseinfo(ui.item);
                         /*$( "#project" ).val( ui.item.label );
                         $( "#project-id" ).val( ui.item.value );
                         $( "#project-description" ).html( ui.item.desc );
@@ -24,13 +39,13 @@ define(['jquery', 'core/config', 'jqueryui', 'core/templates', 'block_searchcour
                         //return false;
                     }
                 }).autocomplete("instance")._renderItem = function (ul, item) {
-                 return $("<li>")
-                     .append("<div class='courseresultnode'>\n" +
-                         "  <span><strong>Full Name: </strong> <span class='label label-info'>"+item.fullname+"</span> </p>\n" +
-                         "    <p><strong> Short Name: </strong><span class='label label-info'> "+item.shortname+" </span> </p>\n" +
-                         "     <p><strong> ID Number: </strong> <span class='label label-info'>"+item.idnumber+" </span> </p>\n" +
-                         "  <p><strong> Category: </strong> <span class='label label-info'>"+item.category+" </span> </p>\n" +
-                         "</div>")
+                return $("<li>")
+                    .append("<div class='courseresultnode'>\n" +
+                        "  <span><strong>Full Name: </strong> <span class='label label-info'>" + item.fullname + "</span> </p>\n" +
+                        "    <p><strong> Short Name: </strong><span class='label label-info'> " + item.shortname + " </span> </p>\n" +
+                        "     <p><strong> ID Number: </strong> <span class='label label-info'>" + item.idnumber + " </span> </p>\n" +
+                        "  <p><strong> Category: </strong> <span class='label label-info'>" + item.category + " </span> </p>\n" +
+                        "</div>")
                     .appendTo(ul);
             };
         }
