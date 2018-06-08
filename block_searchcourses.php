7f4586a7089ad64447501005d2819e6da8c3eb4c
Hittesh Ahuja <h.ahuja@bath.ac.uk>
2018-06-08 15:59:05 +0100

    form is now rendered through a renderer ( mustache template)

--- block_searchcourses.php
+++ block_searchcourses.php
@@ -28,22 +28,19 @@
 /**
  * The Search Courses Autocomplete block class
  */
-class block_searchcourses extends block_base
-{
+class block_searchcourses extends block_base {
 
     /**
      *
      */
-    public function init()
-    {
+    public function init() {
         $this->title = get_string('pluginname', 'block_searchcourses');
     }
 
     /**
      * @return array
      */
-    public function applicable_formats()
-    {
+    public function applicable_formats() {
         return array(
             'site' => true,
             'course-view' => true,
@@ -58,8 +55,7 @@ class block_searchcourses extends block_base
      * {@inheritDoc}
      * @see block_base::is_empty()
      */
-    function is_empty()
-    {
+    function is_empty() {
         if (!has_capability('blocks/searchcourses:view', $this->context)) {
             return true;
         }
@@ -70,8 +66,7 @@ class block_searchcourses extends block_base
     /**
      * @return stdClass
      */
-    public function get_content()
-    {
+    public function get_content() {
         global $CFG, $PAGE;
         $this->content = new stdClass();
         $params = array();
@@ -91,18 +86,6 @@ class block_searchcourses extends block_base
              ));*/
             $count = $this->config->course_count;
         }
-
-        $form_html = "";
-        //$form_html .= $this->page->requires->js_init_call('M.search_autocomplete.init', array(
-        //  $params
-        //), false, $module);
-        /*$form_html .=  html_writer::start_div('', ['id' => 'course_search_ac']);
-        $form_html .= "<label for=\"ac-input\">" . get_string('search_label', 'block_searchcourses') . "</label>";
-        $form_html .= "<input id=\"ac-input\" class=\"typeahead\" type = \"text\" placeholder = \"Start here by typing a course...\"/>";
-        $form_html .= "<div id=\"my_courses_container\"><label for = \"my_courses_flag\">".get_string('my_courses_label', 'block_searchcourses')."</label><input type=\"checkbox\" id=\"my_courses_flag\"  name=\"my_courses_flag\" value=\"0\"/></div>";
-        $form_html .= "<input type=\"hidden\" id=\"course_count\" value=\"$count\" />";*/
-
-        //$form_html .= html_writer::end_div();
 
         //Render from Template
         $form = new \block_searchcourses\output\searchform();
